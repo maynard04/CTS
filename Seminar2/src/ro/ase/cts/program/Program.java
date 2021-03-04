81408e7651374b532ed048fc5fde4b14fa5df7e4
maynard04 <alexmihait98@gmail.com>
2021-03-04 10:37:57 +0200

    Util in Reader classes

---
+++ Seminar2/src/ro/ase/cts/program/Program.java
@@ -0,0 +1,23 @@
+package ro.ase.cts.program;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+import ro.ase.cts.classes.Angajat;
+import ro.ase.cts.readers.AngajatReader;
+
+public class Program {
+
+	public static void main(String[] args) {
+		List<Angajat> listaAngajati;
+		try {
+			listaAngajati = AngajatReader.readAngajati("angajati.txt");
+			for(Angajat angajat:listaAngajati)
+				System.out.println(angajat.toString());
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
