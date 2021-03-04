419aad04d72d0372889f22799c03077ad914ca46
maynard04 <alexmihait98@gmail.com>
2021-03-04 11:31:32 +0200

    Seminar 2

--- Seminar2/src/ro/ase/cts/program/Program.java
+++ Seminar2/src/ro/ase/cts/program/Program.java
@@ -1,19 +1,24 @@
 package ro.ase.cts.program;
-
 import java.io.FileNotFoundException;
 import java.util.List;
-
 import ro.ase.cts.classes.Angajat;
+import ro.ase.cts.classes.Aplicant;
 import ro.ase.cts.readers.AngajatReader;
+import ro.ase.cts.readers.AplicantReader;
+import ro.ase.cts.readers.StudentReader;
 
 public class Program {
+	
+	public static List<Aplicant> readAplicant(String fisier, AplicantReader reader) throws NumberFormatException, FileNotFoundException {
+		return reader.readAplicants(fisier);
+	}
 
 	public static void main(String[] args) {
-		List<Angajat> listaAngajati;
+		List<Aplicant> listaAngajati;
 		try {
-			listaAngajati = AngajatReader.readAngajati("angajati.txt");
-			for(Angajat angajat:listaAngajati)
-				System.out.println(angajat.toString());
+			listaAngajati = readAplicant("studenti.txt", new AplicantReader());
+			for(Aplicant aplicant:listaAngajati)
+				System.out.println(aplicant.toString());
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
--- Seminar2/src/ro/ase/cts/readers/ElevReader.java
+++ Seminar2/src/ro/ase/cts/readers/ElevReader.java
@@ -6,13 +6,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import ro.ase.cts.classes.Aplicant;
 import ro.ase.cts.classes.Elev;
 
-public class ElevReader {
-	public static List<Elev> readPupil(String file) throws FileNotFoundException {
+public abstract class ElevReader extends AplicantReader {
+	public List<Aplicant> readPupil(String file) throws FileNotFoundException {
 		Scanner input2 = new Scanner(new File(file));
 		input2.useDelimiter(",|\n");
-		List<Elev> elevi = new ArrayList<Elev>();
+		List<Aplicant> elevi = new ArrayList<Aplicant>();
 
 		while (input2.hasNext()) {
 			String nume = input2.next();
