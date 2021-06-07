5d0be4553df340af1815f12530653977ac493101
maynard04 <80031112+maynard04@users.noreply.github.com>
2021-06-07 12:38:09 +0300

    Add files via upload

---
+++ Seminar11/src/observer/clase/ClientFidel.java
@@ -0,0 +1,22 @@
+package observer.clase;
+
+public class ClientFidel implements Observer {
+	
+	private String nume;
+	
+
+	public ClientFidel(String nume) {
+		super();
+		this.nume = nume;
+	}
+
+
+	@Override
+	public void primesteMesaj(String mesaj) {
+		System.out.println(nume+" a primit mesajul: "+ mesaj);
+		
+	}
+
+
+
+}
---
+++ Seminar11/src/observer/clase/ManagerSalaAbstract.java
@@ -0,0 +1,30 @@
+package observer.clase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class ManagerSalaAbstract {
+
+	public List<Observer> clientiFideli;
+
+	public ManagerSalaAbstract() {
+		super();
+		this.clientiFideli = new ArrayList<>();
+	}
+	
+	public void adaugaAbonat(Observer abonat) {
+		clientiFideli.add(abonat);
+		
+	}
+	
+	public void stergeAbonat(Observer abonat) {
+		clientiFideli.remove(abonat);
+	}
+	
+	public void trimiteNotificare(String mesaj) {
+		for(Observer client: clientiFideli) {
+			client.primesteMesaj(mesaj);
+		}
+	}
+	
+}
